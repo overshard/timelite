9a7d3ac57f8a4fa9a7013c4f490378383dd3c087
Isaac Bythewood <isaac@bythewood.me>
2019-07-08 06:03:09 +0000

    Remove workbox opts

--- next.config.js
+++ next.config.js
@@ -2,27 +2,7 @@ const withCSS = require("@zeit/next-css");
 const withOffline = require("next-offline");
 
 const nextConfig = {
-  target: "serverless",
-  workboxOpts: {
-    swDest: "static/service-worker.js",
-    runtimeCaching: [
-      {
-        urlPattern: /^https?.*/,
-        handler: "NetworkFirst",
-        options: {
-          cacheName: "https-calls",
-          networkTimeoutSeconds: 15,
-          expiration: {
-            maxEntries: 150,
-            maxAgeSeconds: 30 * 24 * 60 * 60
-          },
-          cacheableResponse: {
-            statuses: [0, 200]
-          }
-        }
-      }
-    ]
-  }
+  target: "serverless"
 };
 
 module.exports = withOffline(withCSS(nextConfig));
