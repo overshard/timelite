97c614d3f1f4537636ab08447d02b213b93264fb
Isaac Bythewood <isaac@bythewood.me>
2019-07-16 02:25:21 +0000

    Add redirect to www on server

--- pages/_document.js
+++ pages/_document.js
@@ -4,9 +4,18 @@ import { ServerStyleSheet } from "styled-components";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
+    // Redirect to www if no www used
+    if (ctx.req.headers.host === "timelite.app") {
+      ctx.res.writeHead(302, {
+        Location: "https://www.timelite.app"
+      });
+      ctx.res.end();
+      return {};
+    }
+
+    // Setup styled-components for rendering server side
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
-
     try {
       ctx.renderPage = () =>
         originalRenderPage({
@@ -36,9 +45,7 @@ class MyDocument extends Document {
             name="description"
             content="Why is it 5 AM? Isn't there something simple I can use to track what I'm doing with all this time?"
           />
-
           <meta name="theme-color" content="#1b1a23" />
-
           <link rel="manifest" href="/manifest.json" />
         </Head>
         <body>
