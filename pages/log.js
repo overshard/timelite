8576ac389b1b58afbad485a0b43d44347b738a77
Isaac Bythewood <isaac@bythewood.me>
2021-04-06 05:46:29 +0000

    Clean up imports

--- pages/log.js
+++ pages/log.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect, useRef } from "react";
+import React, { useContext, useState } from "react";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
 import styled from "styled-components";
 import { CSVLink } from "react-csv";
