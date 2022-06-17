a0a864f9f92928cc6485ee0d9bd40d7f2c6ddd78
Isaac Bythewood <isaac@bythewood.me>
2022-06-17 02:56:37 +0000

    Add next-pwa

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
-const nextConfig = {
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
+
+const nextConfig = withPWA({
   webpack5: true,
-};
+  pwa: {
+    dest: "public",
+    runtimeCaching,
+  },
+});
 
 module.exports = nextConfig;
