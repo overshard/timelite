f188e2df805a6f935a3821a39df726b432c292f2
Isaac Bythewood <isaac@bythewood.me>
2022-06-17 02:28:27 +0000

    Upgrade node deps

--- next.config.js
+++ next.config.js
@@ -1,32 +1,5 @@
-const withOffline = require("next-offline");
-
 const nextConfig = {
-  target: "serverless",
-  workboxOpts: {
-    swDest: process.env.NEXT_EXPORT
-      ? "service-worker.js"
-      : "static/service-worker.js",
-    runtimeCaching: [
-      {
-        urlPattern: /^https?.*/,
-        handler: "NetworkFirst",
-        options: {
-          cacheName: "offlineCache",
-          expiration: {
-            maxEntries: 200,
-          },
-        },
-      },
-    ],
-  },
-  async rewrites() {
-    return [
-      {
-        source: "/service-worker.js",
-        destination: "/_next/static/service-worker.js",
-      },
-    ];
-  },
+  webpack5: true,
 };
 
-module.exports = withOffline(nextConfig);
+module.exports = nextConfig;
