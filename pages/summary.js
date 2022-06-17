2b084a87eb8bec072c628c0c797955129f66e9a8
Isaac Bythewood <isaac@bythewood.me>
2022-06-17 01:02:32 +0000

    Add color to chart

--- pages/summary.js
+++ pages/summary.js
@@ -53,6 +53,14 @@ const Summary = () => {
           {
             label: "# of Hours",
             data: getDatasets(state.log),
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.5)",
+              "rgba(54, 162, 235, 0.7)",
+              "rgba(255, 206, 86, 0.7)",
+              "rgba(75, 192, 192, 0.7)",
+              "rgba(153, 102, 255, 0.7)",
+              "rgba(255, 159, 64, 0.7)",
+            ],
           },
         ],
       },
