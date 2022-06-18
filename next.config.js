84e673e741da2271966a6f2ee03425f4f77a3fa2
Isaac Bythewood <isaac@bythewood.me>
2022-06-18 00:05:29 +0000

    Add next bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,13 +1,24 @@
+const withPlugins = require('next-compose-plugins')
 const withPWA = require("next-pwa");
 const runtimeCaching = require("next-pwa/cache");
+const withBundleAnalyzer = require("@next/bundle-analyzer");
 
-const nextConfig = withPWA({
-  webpack5: true,
-  pwa: {
-    dest: "public",
-    disable: process.env.NODE_ENV === "development",
-    runtimeCaching,
-  },
-});
 
-module.exports = nextConfig;
+module.exports = withPlugins([
+  [
+    withPWA,
+    {
+      pwa: {
+        dest: "public",
+        disable: process.env.NODE_ENV === "development",
+        runtimeCaching,
+      },
+    },
+  ],
+  [
+    withBundleAnalyzer,
+    {
+      enabled: process.env.NODE_ENV === "development",
+    },
+  ],
+]);
